5a1b35d8b9f8d4575e3b0d99d285236b93cfcd30
Vladle1347 <vladleleko@gmail.com>
2021-09-16 10:40:13 +0300

    Version 0.5

---
+++ MyAssistantatCFU/app/src/main/java/com/example/myassistantatcfu/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.myassistantatcfu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.Toast;
+import android.annotation.TargetApi;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button6;
+
+    WebView webView;
+
+    
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        final Activity activity = this;
+
+        webView.setWebViewClient(new WebViewClient() {
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, description, Toast.LENGTH_SHORT).show();
+            }
+            @TargetApi(android.os.Build.VERSION_CODES.M)
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest req, WebResourceError rerr) {
+                // Redirect to deprecated method, so you can use it in all SDK versions
+                onReceivedError(view, rerr.getErrorCode(), rerr.getDescription().toString(), req.getUrl().toString());
+            }
+        });
+
+        webView.loadUrl("https://iseu.cfuv.ru/");
+
+        setContentView(webView);
+    }
+}
---
+++ MyAssistantatCFU/app/src/main/java/com/example/myassistantatcfu/MainActivity3.java
@@ -0,0 +1,19 @@
+package com.example.myassistantatcfu;
+
+import static java.lang.Double.valueOf;
+
+import android.app.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.yandex.mapkit.mapview.MapView;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    public class MapActivity extends Activity {
+        private final String MAPKIT_API_KEY = "68bee2c1-972f-4e31-b634-cbd89a32836a\n";
+        private final String TARGET_LOCATION = ("55.7524392902164,37.62715701062013");
+
+        private MapView mapView;
+    }
+}
