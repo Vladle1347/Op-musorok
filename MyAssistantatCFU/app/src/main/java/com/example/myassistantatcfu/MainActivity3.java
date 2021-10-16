f22da1beae77e1c0c65318541d488fa4143f6a15
Vladle1347 <vladleleko@gmail.com>
2021-10-16 11:14:00 +0300

    попытки блять

--- MyAssistantatCFU/app/src/main/java/com/example/myassistantatcfu/MainActivity3.java
+++ MyAssistantatCFU/app/src/main/java/com/example/myassistantatcfu/MainActivity3.java
@@ -1,19 +1,47 @@
 package com.example.myassistantatcfu;
 
-import static java.lang.Double.valueOf;
-
-import android.app.Activity;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.yandex.mapkit.mapview.MapView;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
 
 public class MainActivity3 extends AppCompatActivity {
 
-    public class MapActivity extends Activity {
-        private final String MAPKIT_API_KEY = "68bee2c1-972f-4e31-b634-cbd89a32836a\n";
-        private final String TARGET_LOCATION = ("55.7524392902164,37.62715701062013");
+    WebView webView;
 
-        private MapView mapView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        final Activity activity = this;
+
+        webView.setWebViewClient(new WebViewClient() {
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, description, Toast.LENGTH_SHORT).show();
+            }
+            @TargetApi(android.os.Build.VERSION_CODES.M)
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest req, WebResourceError rerr) {
+                // Redirect to deprecated method, so you can use it in all SDK versions
+                onReceivedError(view, rerr.getErrorCode(), rerr.getDescription().toString(), req.getUrl().toString());
+            }
+        });
+
+        webView.loadUrl("file:///android_asset/MAP.html");
+
+        setContentView(webView);
+
     }
 }
